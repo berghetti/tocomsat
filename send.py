f476e11cbdf6efbecf4761514a7410c5a029d483
berghetti <40703575+berghetti@users.noreply.github.com>
2020-11-22 15:27:24 -0400

    Update send.py

--- send.py
+++ send.py
@@ -4,5 +4,5 @@ host = '192.168.2.108'
 port = 51234
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
-s.sendall(b'646CB36463')
+s.sendall(b'646CB36463') # code get on encoded.c
 print("code sent")
